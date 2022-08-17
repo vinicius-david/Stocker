251e633bb70d26c9bb00bd654f2dcebb0e522033
vinicius-david <vinicius.davidf@gmail.com>
2022-08-17 20:21:46 -0300

    creating controllers

--- backend/src/routes/sessions.routes.ts
+++ backend/src/routes/sessions.routes.ts
@@ -1,32 +1,11 @@
 import { Router } from 'express';
 
-import AuthUserService from '../services/AuthUserService';
+import SessionsController from '../controllers/SessionsController';
+
+const sessionsController = new SessionsController();
 
 const sessionsRouter: Router = Router();
 
-sessionsRouter.post('/', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const authUser = new AuthUserService();
-
-    const { user, token } = await authUser.execute({
-      email,
-      password,
-    });
-
-    const userWithoutPassword = {
-      ...user,
-      password: null,
-    };
-
-    return res.json({
-      user: userWithoutPassword,
-      token,
-    });
-  } catch (err: any) {
-    return res.status(err.statusCode).json({ error: err.message });
-  }
-});
+sessionsRouter.post('/', sessionsController.create);
 
 export default sessionsRouter;
--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,96 +1,15 @@
 import { Router } from 'express';
 
-import ListUsersService from '../services/ListUsersService';
-import FindUserService from '../services/FindUserService';
-import CreateUserService from '../services/CreateUserService';
-import AddOrRemoveStockService from '../services/AddOrRemoveStockService';
-import UpdateUserService from '../services/UpdateUserService';
+import UsersController from '../controllers/UsersController';
+
+const usersController = new UsersController();
 
 const usersRouter: Router = Router();
 
-usersRouter.get('/', async (req, res) => {
-  try {
-    const listUsers = new ListUsersService();
-
-    const users = await listUsers.execute();
-
-    return res.json(users.map(user => ({ ...user, password: null })));
-  } catch (err: any) {
-    return res.status(400).json({ error: err.message });
-  }
-});
-
-usersRouter.get('/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const findUser = new FindUserService();
-
-    const user = await findUser.execute({ id });
-
-    return res.json({
-      ...user,
-      password: null,
-    });
-  } catch (err: any) {
-    return res.status(400).json({ error: err.message });
-  }
-});
-
-usersRouter.post('/', async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-
-    const createUser = new CreateUserService();
-
-    const user = await createUser.execute({ name, email, password });
-
-    return res.json({
-      ...user,
-      password: null,
-    });
-  } catch (err: any) {
-    return res.status(400).json({ error: err.message });
-  }
-});
-
-usersRouter.patch('/stock/', async (req, res) => {
-  try {
-    const { userId, stockId } = req.body;
-
-    const addOrRemoveStock = new AddOrRemoveStockService();
-
-    const user = await addOrRemoveStock.execute({ userId, stockId });
-
-    return res.json({
-      ...user,
-      password: null,
-    });
-  } catch (err: any) {
-    return res.status(400).json({ error: err.message });
-  }
-});
-
-usersRouter.put('/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const {
-      name, email, password, newPassword,
-    } = req.body;
-
-    const updateUser = new UpdateUserService();
-
-    const user = await updateUser.execute({
-      id, name, email, password, newPassword,
-    });
-
-    return res.json({
-      ...user,
-      password: null,
-    });
-  } catch (err: any) {
-    return res.status(400).json({ error: err.message });
-  }
-});
+usersRouter.get('/', usersController.list);
+usersRouter.get('/:id', usersController.show);
+usersRouter.post('/', usersController.create);
+usersRouter.patch('/stock/', usersController.favorite);
+usersRouter.put('/:id', usersController.update);
 
 export default usersRouter;
